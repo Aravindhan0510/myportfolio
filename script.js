2938ac0d65aa2b1b8d396396d43e378e9a1a0fd7
aravind <aravindgovindaraj777@gmail.com>
2025-01-20 01:50:33 +0530

    Initial changes for my portfolio website

---
+++ script.js
@@ -0,0 +1,41 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent the default form submission
+
+    //Get the values from the form
+    const name = document.getElementById('nameInput').value;
+    const email = document.getElementById('emailInput').value;
+    const message = document.getElementById('messageInput').value;
+
+    //Google Form ID
+    const formId = '1FAIpQLScOuJLfHup8j-ac78o27irAswIGQYpu43_i714LfoFnW2hJGg';
+
+    //Google Form Field IDs
+    const nameId = '1553013261';
+    const emailId = '1183400606';
+    const messageId = '581487933';
+
+    const formData = new FormData();
+    formData.append(`entry.${nameId}`, name);
+    formData.append(`entry.${emailId}`, email);
+    formData.append(`entry.${messageId}`, message);
+
+    const googleFormUrl = `https://docs.google.com/forms/d/e/${formId}/formResponse`;
+
+    fetch(googleFormUrl, {
+        method: 'POST',
+        body: formData,
+    })
+        .then(response => {
+            if (response.ok) {
+                alert("Message Sent Successfully!");
+                document.getElementById('contactForm').reset(); //Reset form after submit
+            } else {
+                alert("There was an error, please try again.");
+                document.getElementById('contactForm').reset(); //Reset form after submit
+            }
+        })
+        .catch(error => {
+            console.error('Error submitting form:', error);
+            alert("There was an error, please try again.");
+        });
+});
